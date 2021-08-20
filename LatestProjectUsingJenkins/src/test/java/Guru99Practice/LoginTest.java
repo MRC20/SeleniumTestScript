94fcd8a35026215f3713abe3efde685c41b97bf9
DeshmuMa <DeshmuMa@FCP-0019519-L.india.rapidigm.com>
2021-08-20 15:53:33 +0530

    ddfff

--- LatestProjectUsingJenkins/src/test/java/Guru99Practice/LoginTest.java
+++ LatestProjectUsingJenkins/src/test/java/Guru99Practice/LoginTest.java
@@ -14,7 +14,7 @@ import java.time.Duration;
 import org.junit.Assert;
 
 public class LoginTest {
-	//test34
+	//test34s
 	WebDriver driver=null;
   @Test(priority=1)
   public void validloginTest() {
